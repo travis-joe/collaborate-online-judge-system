6513c26a7eda464686943039220d977af87376cb
qzy <weelgod@gmail.com>
2018-08-02 07:45:43 +0800

    week2 添加auth0

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -2,16 +2,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-
 import {routing} from './app.routes';
+
+import {DataService} from './services/data.service';
+import {AuthService} from './services/auth.service';
 
 import {AppComponent} from './app.component';
 import {ProblemListComponent} from './components/problem-list/problem-list.component';
-import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
-
-import {DataService} from './services/data.service';
-import { NewProblemComponent } from './components/new-problem/new-problem.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {ProblemDetailComponent} from './components/problem-detail/problem-detail.component';
+import {NewProblemComponent} from './components/new-problem/new-problem.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,11 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
   providers: [{
     provide: 'data',
     useClass: DataService,
-  }],
+    },
+    {
+      provide: 'auth',
+      useClass: AuthService,
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- oj-client/src/app/components/nav-bar/nav-bar.component.ts
+++ oj-client/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   title = 'COJ';
-  username = 'travis joe';
-  constructor() { }
+  username = '';
+  constructor(
+    @Inject('auth') private auth,
+  ) { }
 
   ngOnInit() {
   }
-
+  login(): void {
+    this.auth.login();
+  }
 }
